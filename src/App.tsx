ff73ab892301326ab6deecc6951441c23c36b548
Damian5314 <damianwillmse@gmail.com>
2025-05-04 17:32:43 +0200

    contact page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { PricingPage } from "./pages/PricingPage";
+import { ContactPage } from "./pages/ContactPage";
 import "./index.css";
 // import other pages as you build them
 
 const App: React.FC = () => (
   <Routes>
     <Route path="/" element={<LandingPage />} />
-    {/* future routes:
-      <Route path="/features" element={<FeaturesPage />} />
-      ...
-    */
-    <Route path="/pricing" element={<PricingPage />} />}
+    <Route path="/contact" element={<ContactPage />} />
+    <Route path="/pricing" element={<PricingPage />} />
   </Routes>
 );
 
