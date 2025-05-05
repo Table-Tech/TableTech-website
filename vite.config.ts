95ce680a1c2b1c7a0a977287bd05c3b27155d8c8
Wishant010 <wishantbhajan@outlook.com>
2025-05-05 04:35:55 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),    // ← ensures your CSS passes through Tailwind v4
-  ],
+  base: "/", // ← Voor custom domein, altijd gewoon "/"
+  plugins: [react(), tailwindcss()],
 });
