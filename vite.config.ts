046abfb8326f7f3a20fae9340a41d8820e0642db
Wishant010 <wishantbhajan@outlook.com>
2025-05-05 05:50:31 +0200

    alles

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/", // ← Voor custom domein, altijd gewoon "/"
+  base: "/", // Voor custom domeinen altijd root gebruiken
   plugins: [react(), tailwindcss()],
 });
