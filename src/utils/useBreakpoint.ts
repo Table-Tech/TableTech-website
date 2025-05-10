ab941b7bc35eb95ffbe4541745d95c10732216db
Wishant010 <wishantbhajan@outlook.com>
2025-05-10 20:16:22 +0200

    hook en utils voor respovie netjes werkt

---
+++ src/utils/useBreakpoint.ts
@@ -0,0 +1,19 @@
+// src/hooks/useBreakpoint.ts
+import { useEffect, useState } from "react";
+
+export const useBreakpoint = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return {
+    width,
+    isMobile: width < 640,
+    isTablet: width >= 640 && width < 1024,
+    isDesktop: width >= 1024,
+  };
+};
