09821e5d41f02fa7d2aa662eb6792ecb924ff2f8
Wishant010 <wishantbhajan@outlook.com>
2025-06-02 23:33:27 +0400

    standaard

--- src/pages/LandingPage/Benefits-2.tsx
+++ src/pages/LandingPage/Benefits-2.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import plantenBg from "../../assets/afbeeldingen/Planten.png";
+import plantenBg from "../../assets/afbeeldingen/optie4.png";
 
 export const BenefitsTwo: React.FC = () => {
   return (
-      <section
+    <section
       id="benefits-2"
       className="relative w-full h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat snap-start shrink-0 overflow-hidden"
       style={{ backgroundImage: `url(${plantenBg})` }}
     >
-
       {/* Donkere overlay */}
       <div className="absolute inset-0 bg-[#3b2a1d]/20 z-0" />
 
