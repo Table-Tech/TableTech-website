71e8b2e8b96d1e2d12bbcca6a9090388e97a3237
Wishant010 <144028795+Wishant010@users.noreply.github.com>
2025-07-30 16:06:16 +0200

    algehele fix ik heb geen zin om precies te zsetten wat gedan is

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,21 @@
-// Global type declarations
-
-declare global {
-  interface Window {
-    lenis?: unknown;
-  }
+// src/global.d.ts
+interface Window {
+  lenis?: {
+    scroll: number;
+    scrollTo: (target: number | string | HTMLElement, options?: {
+      offset?: number;
+      duration?: number;
+      easing?: (t: number) => number;
+      immediate?: boolean;
+      lock?: boolean;
+      force?: boolean;
+      onComplete?: () => void;
+    }) => void;
+    on: (event: string, callback: (...args: any[]) => void) => void;
+    off: (event: string, callback: (...args: any[]) => void) => void;
+    raf: (time: number) => void;
+    start: () => void;
+    stop: () => void;
+    destroy: () => void;
+  };
 }
-
-export {};
